6c8271fc2277848d6903d2fde51b91f1bfdac633
apple <nikhiljain1203@gmail.com>
2023-12-07 17:53:03 +0530

    webhook

--- src/main/java/com/example/paymentservicefinal/paymentgateways/PaymentGatewayStrategyChooser.java
+++ src/main/java/com/example/paymentservicefinal/paymentgateways/PaymentGatewayStrategyChooser.java
@@ -21,6 +21,6 @@ public class PaymentGatewayStrategyChooser {
 //        }
 //
 //        return stripePaymentGateway;
-        return razorpayPaymentGateway;
+        return stripePaymentGateway;
     }
 }
